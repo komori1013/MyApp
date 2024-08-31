30fbf387433dc11da638c0d756be8ec13e6e77fd
TAKAHIROKOMORI <takahirokomori0512@gmail.com>
2024-08-31 12:42:09 +0900

    Local Storageの機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -8,43 +8,133 @@ import { TaskBox } from './components/TaskBox';
 import { TodoBoard } from './components/TodoBoard';
 import { Modal2 } from "./components/Modal2";
 import { Finalset } from "./components/finalset";
+import { useEffect } from 'react';
 
-const today = dayjs().format("YYYY-MM-DD");
-
-
+export class Finaltime {
+  id: number;
+  starttime: number; 
+  deadtime: number; 
+  data: string;
+  time: number;
+  place: string;
+constructor(id: number, starttime: number, deadtime: number, data: string, time:number, place:string) {
+  this.id = id;
+  this.starttime= starttime;
+  this.deadtime= deadtime;
+  this.data= data;
+  this.time= time;
+  this.place= place;
+}};
 
 export default function App() {
-  const [modal2, setModal2] =useState(""); 
-  const [modal2_1, setModal2_1] =useState(""); 
-  const [modal2_2, setModal2_2] =useState<number>(0); 
+  const [modal2, setModal2] = useState<string>(""); 
+  const [modal2_1, setModal2_1] = useState<string>(""); 
+  const [modal2_2, setModal2_2] = useState<number>(0); 
   const [show, setShow] = useState<boolean>(false);
   const [show2, setShow2] = useState<boolean>(false);
   const [text, setText] = useState<string>("");
-  const [hPHI, setHPHI] = useState([]);
-  const [hPLI, setHPLI] = useState([]);
-  const [lPHI, setLPHI] = useState([]);
-  const [lPLI, setLPLI] = useState([]);
-  const [eight, setEight] = useState([]);
-  const [nine, setNine] = useState([]);
-  const [ten, setTen] = useState([]);
-  const [eleven, setEleven] = useState([]);
-  const [twelve, setTwelve] = useState([]);
-  const [thirteen, setThirteen] = useState([]);
-  
-  const [fourteen, setFourteen] = useState([]);
-  const [fifteen, setFifteen] = useState([]);
-  const [sixteen, setSixteen] = useState([]);
-  const [seventeen, setSeventeen] = useState([]);
+  const [hPHI, setHPHI] = useState<any[]>([]);
+  const [hPLI, setHPLI] = useState<any[]>([]);
+  const [lPHI, setLPHI] = useState<any[]>([]);
+  const [lPLI, setLPLI] = useState<any[]>([]);
+  const [eight, setEight] = useState<any[]>([]);
+  const [nine, setNine] = useState<any[]>([]);
+  const [ten, setTen] = useState<any[]>([]);
+  const [eleven, setEleven] = useState<any[]>([]);
+  const [twelve, setTwelve] = useState<any[]>([]);
+  const [thirteen, setThirteen] = useState<any[]>([]);
+  const [fourteen, setFourteen] = useState<any[]>([]);
+  const [fifteen, setFifteen] = useState<any[]>([]);
+  const [sixteen, setSixteen] = useState<any[]>([]);
+  const [seventeen, setSeventeen] = useState<any[]>([]);
   const [ListBoxPlace, setListBoxPlace] = useState<string>("");
   const [startLine, setStartLine] = useState<string>("");
   const [deadLine, setDeadLine] = useState<string>("");
-  const [finalData,setFinalData] = useState([]);
+  const [finalData, setFinalData] = useState<Finaltime[]>([]);
   
-  const day = dayjs().format('YYYY-MM-DD')
+  const day = dayjs().format('YYYY-MM-DD');
+
+  class Finaltime {
+    id: number;
+    starttime: number; 
+    deadtime: number; 
+    data: string;
+    time: number;
+    place: string;
   
+    constructor(id: number, starttime: number, deadtime: number, data: string, time: number, place: string) {
+      this.id = id;
+      this.starttime = starttime;
+      this.deadtime = deadtime;
+      this.data = data;
+      this.time = time;
+      this.place = place;
+    }
+  }
+  
+  // Finaltime インスタンスをオブジェクトに変換する関数
+  const convertFinaltimeToObject = (finaltime: Finaltime) => ({
+    id: finaltime.id,
+    starttime: finaltime.starttime,
+    deadtime: finaltime.deadtime,
+    data: finaltime.data,
+    time: finaltime.time,
+    place: finaltime.place,
+  });
+  
+  // オブジェクトを Finaltime インスタンスに変換する関数
+  const convertObjectToFinaltime = (obj: any) => new Finaltime(
+    obj.id,
+    obj.starttime,
+    obj.deadtime,
+    obj.data,
+    obj.time,
+    obj.place
+  );
+
+  const saveToLocalStorage = () => {
+    const data = {
+      finalData: Object.values(finalData).map(final=> convertFinaltimeToObject(final)),
+      modal2,
+      hPHI,
+      lPHI,
+      hPLI,
+      lPLI,
+      modal2_1,
+      modal2_2,
+      show2,
+    };
+    localStorage.setItem('myAppData', JSON.stringify(data));
+    console.log(data);
+  };
+
+  const loadFromLocalStorage = () => {
+    const savedData = localStorage.getItem('myAppData');
+    if (savedData) {
+      const parsedata = JSON.parse(savedData);
+      setFinalData(parsedata.finalData.map(convertObjectToFinaltime));
+      setModal2(parsedata.modal2);
+      setHPHI(parsedata.hPHI);
+      setLPHI(parsedata.lPHI);
+      setHPLI(parsedata.hPLI);
+      setLPLI(parsedata.lPLI);
+      setModal2_1(parsedata.modal2_1);
+      setModal2_2(parsedata.modal2_2);
+      setShow2(parsedata.show2);
+      console.log(parsedata);
+    }
+  };
+
+  const clearFromLocalStorage = () => {
+    localStorage.removeItem('myAppData');
+  };
   return (
     <>
-      <header className="header">{day}</header>
+      <header className="header">{day}
+      <button onClick={saveToLocalStorage}>save</button>
+      <button onClick={loadFromLocalStorage}>load</button>
+      <button onClick={clearFromLocalStorage}>clear</button>
+      </header>
       <body className='container'>
 
         <div className="left-container">
@@ -80,9 +170,3 @@ export default function App() {
     </>
   );
 };
-type finaltime  = {
-  id:number;
-  starttime: string;
-  deadtime: string
-  time:number;
-  }
